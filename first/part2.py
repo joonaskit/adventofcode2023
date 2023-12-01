4869f4f9c9ad6b39565f41eb276f19f4c00d5929
Joonas Kitunen <joonaskitunen@kamk.fi>
2023-12-01 18:47:09 +0200

    Refactoring

--- first/part2.py
+++ first/part2.py
@@ -1,5 +1,7 @@
 from string import digits
 
+INT_DICT = {'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+
 def read(name: str) -> list:
     with open(name, 'r') as file:
         return file.readlines()
@@ -7,34 +9,33 @@ def read(name: str) -> list:
 def get_index(pair: tuple) -> int:
     return pair[0]
 
-def find_first_last(a_str, sub):
+def find_all(line, substring):
     start = 0
     while True:
-        start = a_str.find(sub, start)
+        start = line.find(substring, start)
         if start == -1: return
         yield start
-        start += len(sub)
+        start += len(substring)
+
+def parse_line(line: str) -> list:
+    parts = []
+    for key, value in INT_DICT.items():
+        if key in line:
+            tmp = list(find_all(line, key))
+            for tmp_value in tmp:
+                parts.append((tmp_value, value))
+    for i, char in enumerate(line):
+        if char in digits:
+            parts.append((i, char))
+    parts.sort(key=get_index)
+    return parts
+
 
 def sum(lines: list) -> int:
-    int_dict = {'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
     ints = []
     sum = 0
-    for line in lines:
-        parts = []
-        for key, value in int_dict.items():
-            i = line.find(key)
-            if i >= 0:
-                tmp = list(find_first_last(line, key))
-                if len(tmp) == 1:
-                    parts.append((tmp[0], value))
-                else:
-                    parts.append((tmp[0], value))
-                    parts.append((tmp[-1], value))
-        for i, char in enumerate(line):
-            if char in digits:
-                parts.append((i, char))
-        parts.sort(key=get_index)
-        ints.append(parts)
+    for line in lines:  
+        ints.append(parse_line(line))
     for number in ints:
         if len(number) == 1:
             sum += int(number[0][1] + number[0][1])
