d0eb9683135192c44e3c23dec69501cf0d9e552a
Joonas Kitunen <joonaskitunen@kamk.fi>
2023-12-09 12:56:33 +0200

    added functions for line parsing and calculating next value for line

--- 9th/part1.py
+++ 9th/part1.py
@@ -3,3 +3,9 @@ _EXAMPLE = [[0,3,6,9,12,15], [1,3,6,10,15,21], [10,13,16,21,30,45]]
 def read_file(filename: str) -> list: 
     with open(filename, 'r') as file:
         return file.readlines()
+    
+def parse_line(line: str) -> list:
+    pass
+
+def calculate_next(line: list) -> int: 
+    pass
