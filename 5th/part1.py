301ecfb1bf49b98bd89b86fd95d28c2d34d49670
Joonas Kitunen <joonaskitunen@kamk.fi>
2023-12-09 19:03:07 +0200

    Finished code for day 5 part 1. Input is formatted as a python file

--- 5th/part1.py
+++ 5th/part1.py
@@ -1,5 +1,4 @@
-# testing
-seeds = [79, 14, 55, 13]
+import input
 
 """
 seed-to-soil map:
@@ -8,16 +7,36 @@ seed-to-soil map:
 dest range start, source range start, range length
 """
 
+def read_file(filename: str) -> list:
+    with open(filename, 'r') as file:
+        return file.readlines()
+
+def map_list(seed: int, map: list) -> int: 
+    found = False
+    for seed_range in map: 
+        found, location = map_seed(seed, seed_range)
+        if found:
+            break
+    return location
+
 def map_seed(seed: int, ranges: tuple):
-    source = [i for i in range(ranges[1], ranges[1] + ranges[2])]
-    dest = [i for i in range(ranges[0], ranges[0] + ranges[2])]
-    try:
-        return dest[source.index(seed)]
-    except ValueError:
-        return seed
+    if seed < ranges[1] or seed > ranges[1] + ranges[2]:
+        return False, seed
+    else: 
+        i = seed - ranges[1]
+        return True, ranges[0] + i
 
-def test():
-    for seed in seeds:
-        print(f"Seed: {seed} - dest: {map_seed(seed, (52, 50, 48))}")
-
-test()
+def main():
+    locations = []
+    for seed in input.seeds:
+        tmp = map_list(seed, input.seed_to_soil)
+        tmp = map_list(tmp, input.soil_to_fertilizer)
+        tmp = map_list(tmp, input.fertilizer_to_water)
+        tmp = map_list(tmp, input.water_to_light)
+        tmp = map_list(tmp, input.light_to_temperature)
+        tmp = map_list(tmp, input.temperature_to_humidity)
+        locations.append((map_list(tmp,input.humidity_to_location), seed))
+    lowest = min(locations, key=lambda location: location[0])
+    print(lowest)
+    
+main()
